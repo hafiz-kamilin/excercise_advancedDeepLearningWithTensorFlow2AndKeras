8541461b3f7f0dd75a22e22bdd151a4f6ad97799
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2021-09-17 23:50:47 +0800

    1st commit

---
+++ 1_testTensorFlowInstallation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+while True:
+    option = input("\nDo you want to disable the TensorFlow GPU debug info or not [Yes/No]: ")
+    if option == "Yes" or option == "yes" or option == "y":
+        # to supress tensorflow-gpu debug information
+        import os
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+        break
+    elif option == "No" or option == "no" or option == "n":
+        break
+
+# import the required libraries
+import tensorflow as tf
+
+# sanity test
+print("\nTensorFlow Version: " + str(tf.__version__))
+print("Epsilon value: " + str(tf.keras.backend.epsilon()))
+
+# check if the gpu is enabled or not
+if tf.test.gpu_device_name(): 
+
+    print("\nGPU is accessible by the TensorFlow: {}.\n".format(tf.test.gpu_device_name()))
+
+else:
+
+   print("\nGPU is not accessible / No compatible GPU found.\n")
