0bddf2045a09d10a661ecaaf65c4772270e0a6ce
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2021-12-19 00:43:18 +0800

    Update 01_testTensorFlowInstallation.py

--- source/ch01/01_testTensorFlowInstallation.py
+++ source/ch01/01_testTensorFlowInstallation.py
@@ -9,28 +9,44 @@ Perform 2 sanity check
 
 """
 
-while True:
-    option = input("\nDo you want to disable the TensorFlow GPU debug info or not [Yes/No]: ")
-    if option == "Yes" or option == "yes" or option == "y":
-        # to supress tensorflow-gpu debug information
-        import os
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-        break
-    elif option == "No" or option == "no" or option == "n":
-        break
+def installAndSanityCheck():
 
-# import the required libraries
-import tensorflow as tf
+    try:
 
-# sanity test
-print("\nTensorFlow Version: " + str(tf.__version__))
-print("Epsilon value: " + str(tf.keras.backend.epsilon()))
+        while True:
 
-# check if the gpu is enabled or not
-if tf.test.gpu_device_name(): 
+            option = input("\nDo you want to disable the TensorFlow GPU debug info or not [Yes/No]: ")
+            if option == "Yes" or option == "yes" or option == "y":
+                # to supress tensorflow-gpu debug information
+                import os
+                os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+                break
+            elif option == "No" or option == "no" or option == "n":
+                break
 
-    print("\nGPU is accessible by the TensorFlow: {}.\n".format(tf.test.gpu_device_name()))
+        # import the required libraries
+        import tensorflow as tf
 
-else:
+    except ModuleNotFoundError:
 
-   print("\nGPU is not accessible / No compatible GPU found.\n")
+        print("\nTensorFlow is not installed!")
+
+    else:
+
+        print("\nTensorFlow Version: " + str(tf.__version__))
+        # sanity test
+        epsilon = tf.keras.backend.epsilon()
+        print("Epsilon value is " + str(epsilon) + ", which is " + ("correct." if epsilon == 1e-07 else "false."))
+        # check if the gpu is enabled or not
+        if tf.test.gpu_device_name(): 
+            print("\nGPU is accessible by the TensorFlow: {}.\n".format(tf.test.gpu_device_name()))
+        else:
+            print("\nGPU is not accessible / No compatible GPU found.\n")
+
+########
+# main #
+########
+
+if __name__ == '__main__':
+
+    installAndSanityCheck()
