56162a937ada3497a9cdac916404c52e0b20b956
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2021-12-20 00:00:24 +0800

    Update 01_testTensorFlowInstallation.py

--- source/ch01/01_testTensorFlowInstallation.py
+++ source/ch01/01_testTensorFlowInstallation.py
@@ -9,45 +9,35 @@ Perform 2 sanity check
 
 """
 
-def installAndSanityCheck():
+try:
 
-    try:
+    while True:
 
-        while True:
+        option = input("\nDo you want to disable the TensorFlow GPU debug info or not [Yes/No]: ")
+        if option == "Yes" or option == "yes" or option == "y":
+            # to supress tensorflow-gpu debug information
+            import os
+            os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+            break
+        elif option == "No" or option == "no" or option == "n":
+            break
 
-            option = input("\nDo you want to disable the TensorFlow GPU debug info or not [Yes/No]: ")
-            if option == "Yes" or option == "yes" or option == "y":
-                # to supress tensorflow-gpu debug information
-                import os
-                os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-                break
-            elif option == "No" or option == "no" or option == "n":
-                break
+    print("Loading TensorFlow library")
+    # import the required libraries
+    import tensorflow as tf
 
-        print("Loading TensorFlow library")
-        # import the required libraries
-        import tensorflow as tf
+except ModuleNotFoundError:
 
-    except ModuleNotFoundError:
+    print("\nTensorFlow is not installed!")
 
-        print("\nTensorFlow is not installed!")
+else:
 
+    print("\nTensorFlow Version: " + str(tf.__version__))
+    # sanity test
+    epsilon = tf.keras.backend.epsilon()
+    print("Epsilon value is " + str(epsilon) + ", which is " + ("correct." if epsilon == 1e-07 else "false."))
+    # check if the gpu is enabled or not
+    if tf.test.gpu_device_name(): 
+        print("\nGPU is accessible by the TensorFlow: {}.\n".format(tf.test.gpu_device_name()))
     else:
-
-        print("\nTensorFlow Version: " + str(tf.__version__))
-        # sanity test
-        epsilon = tf.keras.backend.epsilon()
-        print("Epsilon value is " + str(epsilon) + ", which is " + ("correct." if epsilon == 1e-07 else "false."))
-        # check if the gpu is enabled or not
-        if tf.test.gpu_device_name(): 
-            print("\nGPU is accessible by the TensorFlow: {}.\n".format(tf.test.gpu_device_name()))
-        else:
-            print("\nGPU is not accessible / No compatible GPU found.\n")
-
-########
-# main #
-########
-
-if __name__ == '__main__':
-
-    installAndSanityCheck()
+        print("\nGPU is not accessible / No compatible GPU found.\n")
